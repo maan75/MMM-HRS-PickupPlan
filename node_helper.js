1f0fa1a12336cf0accd75d000fdeca0218e7a43f
Martin Andreassen <48354547+maan75@users.noreply.github.com>
2024-12-31 13:18:19 +0100

    Update node_helper.js
    Nye ikoner og oppdatert endpoint

--- node_helper.js
+++ node_helper.js
@@ -10,13 +10,13 @@ const axios = require('axios').default;
 
 module.exports = node_helper.create({
     start: function() {
-    console.log("Starting node_helper for: " + this.name);},
+    console.log("MMM-HRS-PickupPlan: Starting node_helper for: " + this.name);},
     socketNotificationReceived: function (notification, payload) {
         const self = this;
 
 
 	if (notification === "GET_ADRESSE_INFO") {
-		console.log("Inne i GET_ADRESSE_INFO")
+		console.log("MMM-HRS-PickupPlan: Inne i GET_ADRESSE_INFO")
 		axios.get("https://services.webatlas.no/GISLINE.Web.Services.Search.SOLR3.0/Service.svc/json/addressWeighted?searchString=" + payload.config.gateadresse + "&municipality=" + payload.config.kommunenr + "&weightedMunicipality=" + payload.config.kommunenr + "&firstIndex=0&maxNoOfResults=2&language=NO&coordsys=84&clientID=Android-Renovasjon-" + payload.config.kommunenr)
 	 		 .then(function (response){
 				let gatekode = response.data.AddressSearchResult.Roads[0].Id
@@ -25,18 +25,22 @@ module.exports = node_helper.create({
 				self.sendSocketNotification("ADRESSEINFO", {"gateKode": gatekode, "gateNavn" : gatenavn, "husnr" : husnr})						
 						})
 			 .catch(function (error){
+						console.log("MMM-HRS-PickupPlan: Feilet under forsøk på å hente adresseinfo")
 						console.log(error)
-						console.log("Feilet under forsøk på å hente adresseinfo")
+
 						})
 			 .then(function () {
-						console.log("Completed GET_ADRESSE_INFO")
+						console.log("MMM-HRS-PickupPlan: Completed GET_ADRESSE_INFO")
 						})
 		}
 			
 			
 		if (notification === "GET_HRS_PICKUP_PLAN") {
 			var tommeFrakDatoArr = []
-			let AXIOSurl = "https://komteksky.norkart.no/komtek.renovasjonwebapi/api/tommekalender/?kommunenr=" + payload.config.kommunenr + "&gatenavn=" +payload.config.gateNavn+ "&gatekode=" +payload.config.gateKode+ "&husnr=" + payload.config.husnr
+//			let AXIOSurl = "https://komteksky.norkart.no/komtek.renovasjonwebapi/api/tommekalender/?kommunenr=" + payload.config.kommunenr + "&gatenavn=" +payload.config.gateNavn+ "&gatekode=" +payload.config.gateKode+ "&husnr=" + payload.config.husnr
+                        let AXIOSurl = "https://norkartrenovasjon.azurewebsites.net/proxyserver.ashx?server=https://komteksky.norkart.no/MinRenovasjon.Api/api/tommekalender/?kommunenr=" + payload.config.kommunenr + "&gatenavn=" +payload.config.gateNavn+ "&gatekode=" +payload.config.gateKode+ "&husnr=" + payload.config.husnr//
+
+//					https://norkartrenovasjon.azurewebsites.net/proxyserver.ashx?server=https://komteksky.norkart.no/MinRenovasjon.Api/api/tommekalender
 			
 		axios({  
 				method: 'get',
@@ -68,27 +72,30 @@ module.exports = node_helper.create({
 			}	//end response 
 			) //end then
 		.catch(function (error){
+						console.log("MMM-HRS-PickupPlan: Error trying to fetch HRS_PICKUP_PLAN")
 						console.log(error)
-						console.log("Error trying to fetch HRS_PICKUP_PLAN")
 						})
 		.then(function () {
-						console.log("Ending GET_HRS_PICKUP_PLAN")
+						console.log("MMM-HRS-PickupPlan: Ending GET_HRS_PICKUP_PLAN")
 						})
 		}
 		
 		if (notification === "HRS_GET_FRAK_INFO"){
+			console.log("MMM-HRS-PickupPlan: Fetching FRAKINFO"),
 			axios({
 			method : 'get',
-			url: 'https://komteksky.norkart.no/komtek.renovasjonwebapi/api/fraksjoner/',
+			//url: 'https://komteksky.norkart.no/komtek.renovasjonwebapi/api/fraksjoner/',
+			url: 'https://norkartrenovasjon.azurewebsites.net/proxyserver.ashx?server=https://komteksky.norkart.no/MinRenovasjon.Api/api/fraksjoner/',
 			headers: {"kommunenr" : payload.config.kommunenr, "renovasjonappkey" : payload.config.renovasjonappkey}})
 			.then(function (response){
-				//console.log(response.data)
+				console.log(response.data)
 							self.sendSocketNotification("HRS_FRAK_INFO", response.data)
 				})//end function response
 			.catch(function (error){
+				console.log("MMM-HRS-PickupPlan: Error trying to fetch FRAKINFO")
 				console.log(error);
-				console.log("Error trying to fetch FRAKINFO")})
+						})
 			.then(function (){
-				console.log("Completed HRS_GET_FRAK_INFO") })
+				console.log("MMM-HRS-PickupPlan: Completed HRS_GET_FRAK_INFO") })
 			}
 }});
